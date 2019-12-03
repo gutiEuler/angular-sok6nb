b59855d89e2e2aeb073ff0e5ee8a6073113a3975
gutiEuler <gutieuler@gmail.com>
2019-12-03 11:08:07 +0100

    2 files modified

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchService} from '../../services/search.service';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { 
+
+
+  constructor(private searchService:SearchService) { 
 
   }
 
-  
+
 
   ngOnInit() {
   }
