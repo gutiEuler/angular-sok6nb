167353a649d305cb944f46f34ba6b83ceb398347
gutiEuler <gutieuler@gmail.com>
2019-12-04 09:51:33 +0100

    3 files modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { QuestionaryComponent } from './components/questionary/questionary.component';
 
+import { BodyComponent } from './components/body/body.component';
+
 const ROUTES: Routes = [
   {path: 'about', component: AboutComponent },
-  {path: 'questionary', component: QuestionaryComponent}
+  {path: 'questionary', component: QuestionaryComponent},
+  {path: '', component: BodyComponent}
 ];
 
 export const APP_ROUTES: any = RouterModule.forRoot(ROUTES, { useHash: true });
